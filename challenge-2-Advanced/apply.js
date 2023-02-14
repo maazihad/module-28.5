f518aa58f618628936fb5e4076a8c0ba01f457a5
maazihad <maazihadin@gmail.com>
2023-02-14 11:08:53 +0600

    Challenge -3 complete

--- challenge-2-Advanced/apply.js
+++ challenge-2-Advanced/apply.js
@@ -1,1 +1,30 @@
-console.log("welcome");
+function getInputValueById(inputId) {
+   const input = document.getElementById(inputId);
+   const inputValue = input.value;
+   input.value = '';
+   return inputValue;
+};
+function getTextElementAmount(amount) {
+   const amounts = document.getElementById(amount);
+   const amountValue = parseFloat(amounts.innerText);
+   return (amountValue - (amountValue / 100) * 30);
+}
+
+function setTextElementValue(setId, newDiscValue) {
+   const findId = document.getElementById(setId);
+   findId.innerText = newDiscValue;
+}
+
+document.getElementById('apply-button').addEventListener('click', function () {
+
+   const getInput = getInputValueById('coupon-id');
+   const getElementValue = getTextElementAmount('amount-id');
+
+   if (getInput === 'DISC30') {
+      setTextElementValue('disc-amount', getElementValue);
+   }
+   else {
+      alert("You input a wring coupon id. Please write true coupon Id.");
+   }
+
+});
